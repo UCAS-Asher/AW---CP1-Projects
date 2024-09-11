8d852751d0350c7e6d445e04682a97ba31ae46f2
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-11 10:20:14 -0600

    Who are you?

---
+++ whoAreYou/main.py
@@ -0,0 +1,9 @@
+#Asher Wangia, Who are you?
+
+name = input("What is your name?: ")
+
+age = input("How old are you?: ")
+
+color = input("What is your favorite color?: ")
+
+print("Hi", name, "you are", age, "years old and your favorite color is", color)
