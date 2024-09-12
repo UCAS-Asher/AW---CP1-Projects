7fe945f941354f01133185abe2ee53922d48d217
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-12 10:48:14 -0600

    Setting, Resetting, and Resetting

---
+++ awards_tables/main.py
@@ -0,0 +1,20 @@
+#Asher Wangia, Setting, Resetting, and Resetting
+
+staff=32
+
+staff= staff-3
+
+staff= staff+1
+
+students=100
+
+students= students-1
+
+guests= students*2
+
+guests= guests-15
+
+seats=12
+
+
+print((staff+students+guests)/seats)
