260b062152501fe0e4c784cafb59533601146845
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-13 15:53:08 +0000

    Part of proficiency test

---
+++ celcius_to_farenheit/main.py
@@ -0,0 +1,9 @@
+#Asher Wangia, Celsius to Fahrenheit
+
+Celcius = input("What is your tempreature in celcius")
+
+Celcius = int(Celcius)
+
+Farenheit = (Celcius*9/5)+32
+
+print(Celcius, "degrees celcius,",Farenheit, "degrees farenheit" )
