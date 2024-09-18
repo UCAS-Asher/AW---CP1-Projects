de01969a4041c9020d10384cc320d1bb4df64568
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-18 10:49:32 -0600

    Madlib

---
+++ madlib/main.py
@@ -0,0 +1,13 @@
+#Asher Wangia, Madlib
+
+input1 = input("Type a Noun: ")
+
+input2 = input("Type a Verb: ")
+
+input3 = input("Type a Place: ")
+
+input4 = input("Type an Adjective: ")
+
+input5 = input("Type an Emotion: ")
+
+print("Once upon a time there was a", input1, ". The", input1, "was very smart and liked to", input2, ". It lived in a", input3, "which was", input4, ". It had the biggest", input3, "which made everyone", input5, ". The END")
