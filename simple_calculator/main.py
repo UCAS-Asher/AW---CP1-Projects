0904efb548013f06225149f76ec7688c18a761c0
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-20 05:46:41 +0000

    Calculator Revise

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,55 +1,92 @@
 #Asher Wangia, Simple Calculator
 
+#What to type for an opperation
+
+print("Type these for the operations:")
+
+print("+ is for addition")
+
+print("- is for subtraction")
+
+print("* is for multiplication")
+
+print("/ is for division")
+
+print("** is for exponents")
+
+print("// is for floor division")
+
+print("% is for modulus")
+
+print("== is for equal to")
+
+print("!= is for not equal to")
+
+print("> is for greater than")
+
+print("< is for less than")
+
+print(">= is for grater than or equal to")
+
+print("<= is for less than or equal to")
+
+print("abs if for absolute value")
+
+print("round is for rounding")
+
+
+#Inputs
+
 num1 = float(input("Type your first number: "))
 
 operation = (input("Type your operation: "))
 
 num2 = float(input("Type your second number(0 if only one number): "))
 
-if operation == "add":
+  
+#Operations Functioning
+
+if operation == "+":
     print(num1+num2)
  
-if operation == "subtract":
+if operation == "-":
     print(num1-num2)
 
-if operation == "multiply":
+if operation == "*":
     print(num1*num2)
 
-if operation == "division":
+if operation == "/":
     print(num1/num2)
 
-if operation == "exponent":
+if operation == "**":
     print(num1**num2)
 
-if operation == "floor division":
+if operation == "//":
     print(num1//num2)
 
-if operation == "modulus":
+if operation == "%":
     print(num1%num2)
 
-if operation == "equal to":
+if operation == "==":
     print(num1==num2)
 
-if operation == "not equal to":
+if operation == "!=":
     print(num1!=num2)
 
-if operation == "greater than":
+if operation == ">":
     print(num1>num2)
 
-if operation == "less than":
+if operation == "<":
     print(num1<num2)
 
-if operation == "greater than or equal to":
+if operation == ">=":
     print(num1>=num2)
 
-if operation == "less than or equal to":
+if operation == "<=":
     print(num1<=num2)
 
-if operation == "absolute value":
+if operation == "abs":
     print(abs(num1))
 
 if operation == "round":
     print(round(num1,int(num2)))
-
-if operation == "modulus":
-    print(num1%num2)
