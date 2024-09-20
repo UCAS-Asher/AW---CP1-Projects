e1b46d1a0a355777e05ad588dc3fb817d8801e3c
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-20 10:44:54 -0600

    Information Coverter

---
+++ personal_information/main.py
@@ -0,0 +1,19 @@
+#Asher Wangia, Personal Information Converter
+
+print("You will take a quiz asking you Questions")
+
+name = input("What is your Name: ")
+
+age = input("How old are you: ")
+
+height = input("How tall are you in Meters: ")
+
+number = input("What is your Favorite Number: ")
+
+print(name)
+
+print(age,  "years old,", int(age), "years old")
+
+print(height,  "meters tall,", float(height), "meters tall")
+
+print("Favorite Number is",number, ",",  "Favorite Number is", int(number))
