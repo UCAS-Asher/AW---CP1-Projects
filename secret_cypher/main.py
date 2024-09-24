b7b03b9249819b7fd1e8520492c290d6e32a8072
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-24 10:49:57 -0600

    Part of cypher

---
+++ secret_cypher/main.py
@@ -0,0 +1,12 @@
+#Asher Wangia, Secret Cypher
+
+print("This Will Translate Your Message To A Secret Code")
+string = input("What is your message: ").lower
+shifts = int(input("How many places will you shift:"))
+
+def cypher(string):
+
+    string = (string)
+    return f"This is your coded secret message.",string
+
+print(ord("z"))
