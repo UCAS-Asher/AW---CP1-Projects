0f822af74641b44b61bfe4b516ecf115ca853854
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-09-25 03:13:54 +0000

    Part Cypher

--- secret_cypher/main.py
+++ secret_cypher/main.py
@@ -1,12 +1,22 @@
 #Asher Wangia, Secret Cypher
 
 print("This Will Translate Your Message To A Secret Code")
+
 string = input("What is your message: ").lower
+
 shifts = int(input("How many places will you shift:"))
+def char(string, firstletter):
+    if string.isalpha():
+        firstletter = "a"
+        old = ord(string) - ord(firstletter)
+        new = (old + shifts)
+        return chr(new + ord(firstletter))
+    return string
 
-def cypher(string):
 
-    string = (string)
-    return f"This is your coded secret message.",string
-
-print(ord("z"))
+def cypher(string, shifts):
+    cipher = " "
+    cipher+= char(string, shifts)
+    return cipher
+cipher = cypher(string, shifts)
+print(f"Cipher: {cipher}")
