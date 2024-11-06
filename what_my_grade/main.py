931952a13842eeb5165513ed1bcf60b447e1dfcd
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-11-06 01:42:29 +0000

    Finished

--- what_my_grade/main.py
+++ what_my_grade/main.py
@@ -3,30 +3,35 @@ print("This is a grade checker")
 grade_check = True
 
 while grade_check == True:
-    class_name = input("What is Your Class Name: ")
-    grade = float(input("What is your Grade in This Class: "))
-
-    if grade >= 95:
-        print("You have an A")
-    elif grade < 95 and grade >= 90:
-        print("You have an A-")
-    elif grade < 90 and grade >= 87:
-        print("You have a B+")
-    elif grade < 87 and grade >= 85:
-        print("You have a B")
-    elif grade < 85 and grade >= 80:
-        print("You have a B-")
-    elif grade < 80 and grade >= 77:
-        print("You have a C+")
-    elif grade < 77 and grade >= 75:
-        print("You have a C")
-    elif grade < 75 and grade >= 70:
-        print("You have a C-")
-    elif grade < 70 and grade >= 67:
-        print("You have a D+")
-    elif grade < 67 and grade >= 65:
-        print("You have a D")
-    elif grade < 65 and grade >= 60:
-        print("You have a D-")
+    choice = input("Are you done checking your grades yes or no: ")
+    if choice == "yes":
+        print("Have a nice day!")
+        break
     else:
-        print("You have an F")
+        class_name = input("What is Your Class Name: ")
+        grade = float(input("What is your Grade in This Class: "))
+
+        if grade >= 95:
+            print("You have an A")
+        elif grade < 95 and grade >= 90:
+            print("You have an A-")
+        elif grade < 90 and grade >= 87:
+            print("You have a B+")
+        elif grade < 87 and grade >= 85:
+            print("You have a B")
+        elif grade < 85 and grade >= 80:
+            print("You have a B-")
+        elif grade < 80 and grade >= 77:
+            print("You have a C+")
+        elif grade < 77 and grade >= 75:
+            print("You have a C")
+        elif grade < 75 and grade >= 70:
+            print("You have a C-")
+        elif grade < 70 and grade >= 67:
+            print("You have a D+")
+        elif grade < 67 and grade >= 65:
+            print("You have a D")
+        elif grade < 65 and grade >= 60:
+            print("You have a D-")
+        else:
+            print("You have an F")
