63d10945ebd9e00bef4e20efdd994e6dcccf2095
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-11-06 10:51:14 -0700

    Multi Table

---
+++ multi_table/main.py
@@ -0,0 +1,20 @@
+#Asher Wangia, Multiplication Table
+
+multi = (input("What number would you like multiples: "))
+
+for x in multi:
+    x = int(x)
+    print(x*1)
+    print(x*2)
+    print(x*3)
+    print(x*4)
+    print(x*5)
+    print(x*6)
+    print(x*7)
+    print(x*8)
+    print(x*9)
+    print(x*10)
+    print(x*11)
+    print(x*12)
+else:
+    print("These are your multiple from 1-12")
