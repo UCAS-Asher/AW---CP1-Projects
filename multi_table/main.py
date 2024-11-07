6590d59c4da7a26f4278cc63b6ab4820d85451bb
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-11-07 10:19:55 -0700

    Revised Multi Table

--- multi_table/main.py
+++ multi_table/main.py
@@ -1,20 +1,8 @@
 #Asher Wangia, Multiplication Table
 
-multi = (input("What number would you like multiples: "))
-
-for x in multi:
-    x = int(x)
-    print(x,"* 1: ",x*1)
-    print(x,"* 2: ",x*2)
-    print(x,"* 3: ",x*3)
-    print(x,"* 4: ",x*4)
-    print(x,"* 5: ",x*5)
-    print(x,"* 6: ",x*6)
-    print(x,"* 7: ",x*7)
-    print(x,"* 8: ",x*8)
-    print(x,"* 9: ",x*9)
-    print(x,"* 10: ",x*10)
-    print(x,"* 11: ",x*11)
-    print(x,"* 12: ",x*12)
+multi = float(input("What number would you like multiples: "))
+times = [1,2,3,4,5,6,7,8,9,10,11,12]
+for x in times :
+    print(multi,"times",x,": ",multi*x)
 else:
     print("These are your multiple from 1-12")
