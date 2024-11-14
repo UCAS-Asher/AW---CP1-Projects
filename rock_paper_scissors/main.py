d38f831fd81dcceeee923308bc1522712c8377a8
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-11-14 07:10:43 +0000

    rock paper scissors

--- rock_paper_scissors/main.py
+++ rock_paper_scissors/main.py
@@ -5,13 +5,33 @@ import random
 
 play = True
 player_score = 0
-cpu_score = 0
+comp_score = 0
 comp_choices = ["rock","paper","scissors"]
 
-def cpu():
-    global comp_choice
-    comp_choice = random.choice(comp_choices)
-
 while play == True:
-    cpu()
-print(comp_choice)
+    comp_choice = random.choice(comp_choices)
+    user_choice = str(input("Pick rock, paper or scissors or exit to quit: "))
+    
+    if user_choice != ("rock" or "paper" or "scissors" or "exit"):
+        print("invalid choice")
+        continue
+    else:
+        if user_choice == comp_choice:
+            print("Its a Tie")
+        elif user_choice == "rock" and comp_choice == "scissors":
+            print("User Won")
+            player_score +=1
+        elif user_choice == "scissors" and comp_choice == "paper":
+            print("User Won")
+            player_score +=1
+        elif user_choice == "paper" and comp_choice == "rock":
+            print("User Won")
+            player_score +=1
+        elif user_choice == "exit":
+            print("Player Score:", player_score)
+            print("Computer Score:", comp_score)
+            play = False
+            break
+        else:
+            print("Computer Won")
+            comp_score +=1
