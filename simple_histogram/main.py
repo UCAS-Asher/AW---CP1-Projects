b0eef5e3eccbb47530e9a50ecac7c5960580bb72
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-11-23 03:21:26 +0000

    Histogram

--- simple_histogram/main.py
+++ simple_histogram/main.py
@@ -1,7 +1,22 @@
 #Asher Wangia, Simple Histogram
 
-num_list = list(input("Type more than 5 numbers seperated by ',': "))
-print(num_list)
+num_done = False
 
-for x in num_list:
-    print()
+num_list = []
+
+while num_done == False:
+
+    number = input("Type a number: ")
+
+    num_list.append((number))
+
+    num_length = len(num_list)
+    
+    if num_list[5:]:
+        choice = input("Do you want to stop Yes or No: ")
+        if choice == "Yes":
+            num_done = True
+
+
+for num in num_list:
+        print(num,"=","*"*int(num))
