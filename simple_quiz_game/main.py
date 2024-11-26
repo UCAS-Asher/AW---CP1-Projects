ad2b851c0e56b365a67e745d7b1f7123da776707
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-11-26 07:25:34 +0000

    Quiz Game

--- simple_quiz_game/main.py
+++ simple_quiz_game/main.py
@@ -1,47 +1,297 @@
 #Asher Wangia, Simple Quiz Game
 import random
-
-print("This is a Math Quiz For Multiplication")
-
 quiz = True
+quiz_a = True
+quiz_b = False
+quiz_c = False
+quiz_d = False
+
+question_num = 0
 user_score = 0
-quiz_total = 5
 
 while quiz == True:
+  
     
-    print("""Q1. 21 times 34
-      A. 712
-      B. 714
-      C. 672
-      D. 724""")
-    
-    user_answer = input("Choose from one of the Choices: ")
-    
-    if user_answer == "B":
-        ("Correct Answer")
-        user_score +=1
-    else:
-        print("Wrong Answer")
+    while quiz_a == True:
+        if question_num >= 5:
+            quiz_a = False
+            print("Your Score was", (user_score/5)*100,"% or", user_score,"out of 5")
+            break
         
-        print("""Q2. 12 times 11
-        A. 132
-        B. 112
-        C. 121
-        D. 143""")
+        num1 = random.choice(range(20,30,2))
+        num2 = random.choice(range(25,35))
+        question_num +=1
+    
+        answ1 = (num1*num2)-11
+        answ2 = num1*num2
+        answ3 = (num1*num2)-5
+        answ4 = (num1*num2)+7
+    
+        questions = [answ1,answ2,answ3,answ4]
+    
+        a = random.choice(questions)
+        questions.remove(a)
+    
+        b = random.choice(questions)
+        questions.remove(b)
+
+        c = random.choice(questions)
+        questions.remove(c)
+
+        d = random.choice(questions)
+        questions.remove(d)
+
+        print("Question",question_num, "What is", num1, "times", num2,"?")
         
-        user_answer = input("Choose from one of the Choices: ")
+        print("A.", a)
+        print("B.", b)
+        print("C.", c)
+        print("D", d)
+        
+        user_answer = input("Choose from the options: ")
+
         if user_answer == "A":
-            user_score +=1
-
-    print("""Q. 12 times 11
-    A. 132
-    B. 112
-    C. 121
-    D. 143""")
+            if a == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_a = False
+                quiz_b = True
+        elif user_answer == "B":
+            if b == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_a = False
+                quiz_b = True
+        elif user_answer == "C":
+            if c == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_a = False
+                quiz_b = True
+        elif user_answer == "D":
+            if d == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_a = False
+                quiz_b = True
         
-    user_answer = input("Choose from one of the Choices: ")
+
+
+        
+    while quiz_b == True:
+        if question_num >= 5:
+            quiz_b = False
+            print("Your Score was", (user_score/5)*100,"% or", user_score,"out of 5")
+            break
+        
+        num1 = random.choice(range(10,15,2))
+        num2 = random.choice(range(15,20))
+        question_num +=1
+        
+        answ1 = (num1*num2)-11
+        answ2 = num1*num2
+        answ3 = (num1*num2)-5
+        answ4 = (num1*num2)+7
+        
+        questions = [answ1,answ2,answ3,answ4]
+        
+        a = random.choice(questions)
+        questions.remove(a)
+
+        b = random.choice(questions)
+        questions.remove(b)
+
+        c = random.choice(questions)
+        questions.remove(c)
+
+        d = random.choice(questions)
+        questions.remove(d)
+
+        print("Question",question_num, "What is", num1, "times", num2,"?")
+        
+        print("A.", a)
+        print("B.", b)
+        print("C.", c)
+        print("D", d)
+        
+        user_answer = input("Choose from the options: ")
+
+        if user_answer == "A":
+            if a == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_b = False
+                quiz_c = True
+        elif user_answer == "B":
+            if b == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_b = False
+                quiz_c = True
+        elif user_answer == "C":
+            if c == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_b = False
+                quiz_c = True
+        elif user_answer == "D":
+            if d == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_b = False
+                quiz_c = True
+        
 
 
 
+    while quiz_c == True:
+        if question_num >= 5:
+            quiz_c = False
+            print("Your Score was", (user_score/5)*100,"% or", user_score,"out of 5")
+            break
+        
+        num1 = random.choice(range(5,10,2))
+        num2 = random.choice(range(10,15))
+        question_num +=1
+        
+        answ1 = (num1*num2)-11
+        answ2 = num1*num2
+        answ3 = (num1*num2)-5
+        answ4 = (num1*num2)+7
+        
+        questions = [answ1,answ2,answ3,answ4]
+        
+        a = random.choice(questions)
+        questions.remove(a)
 
-print("Your Score was", (user_score/quiz_total)*100,"% or", user_score,"out of 5")
+        b = random.choice(questions)
+        questions.remove(b)
+
+        c = random.choice(questions)
+        questions.remove(c)
+
+        d = random.choice(questions)
+        questions.remove(d)
+
+        print("Question",question_num, "What is", num1, "times", num2,"?")
+        
+        print("A.", a)
+        print("B.", b)
+        print("C.", c)
+        print("D", d)
+        
+        user_answer = input("Choose from the options: ")
+        
+        if user_answer == "A":
+            if a == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_c = False
+                quiz_d = True
+        elif user_answer == "B":
+            if b == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_c = False
+                quiz_d = True
+        elif user_answer == "C":
+            if c == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_c = False
+                quiz_d = True
+        elif user_answer == "D":
+            if d == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+                quiz_c = False
+                quiz_d = True
+
+
+    while quiz_d == True:
+        if question_num >= 5:
+            quiz_d = False
+            print("Your Score was", (user_score/5)*100,"% or", user_score,"out of 5")
+            break
+        
+        num1 = random.choice(range(0,5))
+        num2 = random.choice(range(5,10))
+        question_num +=1
+        
+        answ1 = (num1*num2)-11
+        answ2 = num1*num2
+        answ3 = (num1*num2)-5
+        answ4 = (num1*num2)+7
+        
+        questions = [answ1,answ2,answ3,answ4]
+        
+        a = random.choice(questions)
+        questions.remove(a)
+
+        b = random.choice(questions)
+        questions.remove(b)
+
+        c = random.choice(questions)
+        questions.remove(c)
+
+        d = random.choice(questions)
+        questions.remove(d)
+
+        print("Question",question_num, "What is", num1, "times", num2,"?")
+        
+        print("A.", a)
+        print("B.", b)
+        print("C.", c)
+        print("D", d)
+        
+        user_answer = input("Choose from the options: ")
+
+        if user_answer == "A":
+            if a == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+        elif user_answer == "B":
+            if b == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+        elif user_answer == "C":
+            if c == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+        elif user_answer == "D":
+            if d == num1*num2:
+                user_score +=1
+                print("Correct Answer")
+            else:
+                print("Wrong Answer")
+        
