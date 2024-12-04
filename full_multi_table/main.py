fdb3d9285a5228b0904e121bf749a979db348323
UCAS-Asher <asher.wangia@ucas-edu.net>
2024-12-04 07:14:19 +0000

    multi table

--- full_multi_table/main.py
+++ full_multi_table/main.py
@@ -1,13 +1,8 @@
 #Asher Wangia, Full Multiplication Table
 
-times = [1,2,3,4,5,6,7,8,9,10,11,12]
+num = 12
 
-num = 1
-
-while num <= 12:
-    for x in times:
-        print(num," |", num*1, "|", num*2, "|", num*3, "|", num*4, "|", num*5, "|", num*6, "|", num*7, "|", num*8, "|", num*9, "|", num*10, "|", num*11, "|", num*12, "|")
-        num+=1
-
-
-
+for row in range(1,13): 
+    for collum in range(1,13): 
+        print(f"{row * collum:4}", end =" ") 
+    print() 
